725763449ea9e5285343219dbbeacdde786fef39
Mdperez19 <mdperezpriego@gmail.com>
2024-01-14 23:28:57 -0600

    Refactor topKFrequent method to use a PriorityQueue

--- medium/347.top-k-frequent-elements.java
+++ medium/347.top-k-frequent-elements.java
@@ -7,22 +7,27 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> frequencyMap = new HashMap<>();
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
 
-        for (int num : nums) {    
+        for (int num : nums) {
             if (!frequencyMap.containsKey(num))
                 frequencyMap.put(num, 1);
             else
-                frequencyMap.put(num, frequencyMap.get(num)+1);
-
+                frequencyMap.put(num, frequencyMap.get(num) + 1);
         }
 
-        return frequencyMap.entrySet().stream()
-        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-        .limit(k)
-        .mapToInt(Map.Entry::getKey)
-        .toArray();
+        Queue<Integer> topFrequents = new PriorityQueue<>(Comparator.comparingInt(frequencyMap::get)
+                                                                    .reversed());
+        for (int num : frequencyMap.keySet()) {
+            topFrequents.add(num);
+        }
 
+        int[] topKfrequency = new int[k];
+        for (int i = 0; i < k; i++) {
+            topKfrequency[i] = topFrequents.poll();
+        }
+        
+        return topKfrequency;
     }
 }
 // @lc code=end
